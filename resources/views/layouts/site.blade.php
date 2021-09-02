da4c39067274c1b445c9615ae7bacdac9ab4b0f3
Javier Herrera <javier.ivancouoh@gmail.com>
2021-09-02 16:51:14 -0500

    Fix when a category has not articles

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -53,8 +53,8 @@
         for (let index = 0; index < dataLength; index++) {
             const data = response.data[index];
             content += `
-                    <div class="item" onclick="filterArticlesByCategoryId(this, ${data.id})" >${data.name}</div>
-                `;
+                        <div class="item" onclick="filterArticlesByCategoryId(this, ${data.id})" >${data.name}</div>
+                    `;
         }
 
         getId.innerHTML = content;
@@ -87,7 +87,6 @@
 
             for (let i = 0; i < long; i++) {
                 const data = response.data[i];
-
                 let coverImage = data.image.includes('http') ? data.image : `storage/${data.image}`;
 
                 element += `
@@ -106,7 +105,12 @@
             };
 
             getIdContent.innerHTML = element;
-        });
+        }).catch(error => {
+            const getIdContent = document.getElementById('recent-articles');
+            let element = '';
+            element += `<p>No se encontraron noticias</p>`
+            getIdContent.innerHTML = element;
+        })
     };
 </script>
 
